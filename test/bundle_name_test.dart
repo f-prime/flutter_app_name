f155bb4c6d8e265b752bdb113e14d3b8fab5d5f1
zelenin.andrew <zelenin.andrew@gmail.com>
2022-07-02 16:25:05 +0300

    Updated tests

--- test/bundle_name_test.dart
+++ test/bundle_name_test.dart
@@ -73,6 +73,56 @@ void main() {
 </manifest>
   """;
 
+  final String iosPlistDifferentNames =
+      r"""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleDevelopmentRegion</key>
+	<string>$(DEVELOPMENT_LANGUAGE)</string>
+	<key>CFBundleDisplayName</key>
+	<string>Flutter App Name</string>
+	<key>CFBundleExecutable</key>
+	<string>$(EXECUTABLE_NAME)</string>
+	<key>CFBundleIdentifier</key>
+	<string>$(PRODUCT_BUNDLE_IDENTIFIER)</string>
+	<key>CFBundleInfoDictionaryVersion</key>
+	<string>6.0</string>
+	<key>CFBundleName</key>
+	<string>flutter app</string>
+	<key>CFBundlePackageType</key>
+	<string>APPL</string>
+	<key>CFBundleShortVersionString</key>
+	<string>$(MARKETING_VERSION)</string>
+	<key>CFBundleSignature</key>
+	<string>????</string>
+	<key>CFBundleVersion</key>
+	<string>$(CURRENT_PROJECT_VERSION)</string>
+	<key>LSRequiresIPhoneOS</key>
+	<true/>
+	<key>UILaunchStoryboardName</key>
+	<string>LaunchScreen</string>
+	<key>UIMainStoryboardFile</key>
+	<string>Main</string>
+	<key>UISupportedInterfaceOrientations</key>
+	<array>
+		<string>UIInterfaceOrientationPortrait</string>
+		<string>UIInterfaceOrientationLandscapeLeft</string>
+		<string>UIInterfaceOrientationLandscapeRight</string>
+	</array>
+	<key>UISupportedInterfaceOrientations~ipad</key>
+	<array>
+		<string>UIInterfaceOrientationPortrait</string>
+		<string>UIInterfaceOrientationPortraitUpsideDown</string>
+		<string>UIInterfaceOrientationLandscapeLeft</string>
+		<string>UIInterfaceOrientationLandscapeRight</string>
+	</array>
+	<key>UIViewControllerBasedStatusBarAppearance</key>
+	<false/>
+</dict>
+</plist>
+  """;
+
   final String iosPlist = r"""<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -193,13 +243,28 @@ void main() {
 
   test("iOS", () {
     expect(
-      ios.fetchCurrentBundleName(context, iosPlist),
-      equals('<string>Flutter App Name</string>'),
+      ios.fetchCurrentBundleNames(context, iosPlist),
+      equals({
+        "CFBundleName": '<string>Flutter App Name</string>',
+        "CFBundleDisplayName": "<string>Flutter App Name</string>"
+      }),
     );
 
     expect(
       ios.setNewBundleName(
-          context, iosPlist, '<string>Flutter App Name</string>', "Test"),
+          context,
+          iosPlist,
+          {
+            "CFBundleName": '<string>Flutter App Name</string>',
+            "CFBundleDisplayName": "<string>Flutter App Name</string>"
+          },
+          "Test"),
+      equals(iosPlistUpdated),
+    );
+
+    expect(
+      ios.setNewBundleName(context, iosPlistDifferentNames,
+          ios.fetchCurrentBundleNames(context, iosPlistDifferentNames), "Test"),
       equals(iosPlistUpdated),
     );
   });
